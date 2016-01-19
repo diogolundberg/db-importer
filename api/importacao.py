25d23e9d5a9602d991ca220191879513d625f346
Diogo Lundberg <diogo@lundberg.com.br>
2016-01-18 22:58:19 -0200

    Removing outdated docstrings from importacao.py

--- api/importacao.py
+++ api/importacao.py
@@ -1,6 +1,3 @@
-""" Esse módulo contém funções de importação, responsáveis por realizar a
-transferência de dados e controlar as threads para importação distribuida.
-"""
 import time
 import threading
 from source import db
@@ -10,47 +7,12 @@ from api.sql import insert
 
 
 def usuario_importacao(database=db.SASC):
-    """ Esta função é utilizada pelo projeto sempre que o usuario de importação
-    precisar ser informado. Ele será sempre selecionado da tb_usuario e no banco
-    fornecido.
-
-    Args:
-        database (Config): Configurações do banco de dados aonde está a tabela
-        de usuários.
-
-    Returns:
-        A id do usuário chamado 'importacao' da tabela 'tb_usuario'
-    """
     cursor = db.cursor(database)
     return cursor.execute_scalar('select id from tb_usuario where usuario = %s',
                                  'importacao')
 
 
 def executar_importacao(importavel, offset, limit):
-    """ Função que executa a importação. Deve selecionar os dados do database
-    origem do importável e inserir no banco de dados destino. As tuplas importadas
-    do banco devem estar dentro do limit e offset.
-
-    Args:
-        importavel (Importavel): Um objeto da classe Importavel.
-        offset (int): Este é o número do ponto de partida da importação.
-        limit (int): É o número de tuplas máximo esse método irá importar.
-
-    Yields:
-        A importação é executada seguindo esses passos:
-            1 - O cursor trará os dados como uma lista de tuplas do banco de dados
-            definido em importavel.database_select
-            2 - Realizando um iteração para cada tupla, será chamado o método
-            'dados()' do importável, para ter o mapa que corresponde ao 'de para'
-            desta tupla.
-            3 - As chaves desse mapa devem ser armazenadas em ordem, na lista
-            declarada como 'colunas'.
-            4 - Os valores desse mapa devem ser armazenadas em formato de tupla,
-            em ordem, na lista declarada como 'tuplas'.
-            5 - Com a lista de colunas e tuplas, pode-se utilizar o método
-            'insert' deste módulo, para realizar a inserção dos dados na tabela
-            configurada em importavel.tabela.
-    """
     tuplas = []
     colunas = []
 
@@ -74,18 +36,6 @@ def executar_importacao(importavel, offset, limit):
 
 
 def thread_importador(importavel, fila):
-    """ É uma thread, cada thread criada com esse método deve executar os trabalhos
-    a fila, até esvazia-la.
-
-    Args:
-        importavel (Importavel): Um objeto da classe Importavel.
-        fila (Queue): É uma fila segura para threads, contendo várias faixas de
-        importação. Cada faixa indicará qual o trabalho da thread.
-
-    Yields:
-        Define como cada thread funcionará. Cada thread deve repetir o processo
-        de pegar uma item na fila e resovê-lo.
-    """
     while True:
         item = fila.get()
         if item:
@@ -101,27 +51,6 @@ def distribuir_importacao(importavel,
                           ultima_row,
                           numero_threads,
                           tamanho_fila):
-    """ Função que cria fila de tarefas e inicia as threads que irão realiza-las.
-    O número de threads e o tamanho da fila são dinâmicos, e o começo e o fim dos
-    arquivos que serão manipulados.
-
-    Args:
-        importavel (Importavel): Um objeto da classe Importavel.
-        primeira_row (int): Primeira linha do select no banco de dados a ser
-        importada.
-        ultima_row (int): Última linha do select no banco de dados a ser
-        importada.
-        numero_threads (int): O número de threads paralelas.
-        tamanho_fila (int): É a quantidade total de tarefas que o importador
-                            será divido.
-
-    Yields:
-        Este método divide a execução da importação em várias faixas de acordo
-        com o tamanho da fila e respeitado a primeira e ultima linha.
-        Essas faixas são colocadas em um fila segura para threads.
-        Com a fila pronta, cria-se o número de threads informado por parâmetro.
-        Essas threads funcionarão até que todas as tarefas estejam feitas.
-    """
     offset = primeira_row - 1
     total = ultima_row - offset
 
